4f643703a0b7a4a0a669b75e8528f814f0d299df
Karlita <karlis58rg@gmail.com>
2020-05-19 17:39:56 -0500

    login completo

--- app/src/main/java/mx/hgo/reglamento/LoginUser.java
+++ app/src/main/java/mx/hgo/reglamento/LoginUser.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -20,49 +21,51 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class Login extends AppCompatActivity {
-    Button btnIniciarL;
-    EditText txtUserL,txtPassL;
-    String user,pass,respUser,cargarInfoUsuario,cargarInfoLogin;
-    int bandera;
+public class LoginUser extends AppCompatActivity {
+    EditText txtPassUserLU;
+    TextView lblUsuarioLU;
+    String user,pass,respPass,cargarInfoUser;
     SharedPreferences share;
     SharedPreferences.Editor editor;
+    Button btnEntrarLU;
+    int banderaLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_user);
+        cargarDatos();
+        lblUsuarioLU = findViewById(R.id.lblUsuario);
+        txtPassUserLU = findViewById(R.id.txtPassUser);
+        btnEntrarLU = findViewById(R.id.btnEntrar);
 
-        btnIniciarL = findViewById(R.id.btnIniciar);
-        txtUserL = findViewById(R.id.txtUser);
-        txtPassL = findViewById(R.id.txtPass);
+        lblUsuarioLU.setText(cargarInfoUser);
 
-        btnIniciarL.setOnClickListener(new View.OnClickListener() {
+        btnEntrarLU.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(txtUserL.getText().toString().isEmpty()){
-                    Toast.makeText(getApplicationContext(),"EL USUARIO ES NECESARIO PARA INGRESAR",Toast.LENGTH_SHORT).show();
-                }else if(txtPassL.getText().toString().isEmpty()){
-                    Toast.makeText(getApplicationContext(),"LA CONTRASEÑA ES NECESARIA PARA INGRESAR",Toast.LENGTH_SHORT).show();
-                }else if(txtUserL.getText().length() < 3){
-                    Toast.makeText(getApplicationContext(),"EL USUARIO NO PUEDE SER MENOR A TRES LETRAS",Toast.LENGTH_SHORT).show();
-                }else if(txtPassL.getText().length() < 3){
-                    Toast.makeText(getApplicationContext(),"LA CONTRASEÑA NO PUEDE SER MENOR A TRES LETRAS",Toast.LENGTH_SHORT).show();
+                if(txtPassUserLU.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"SU CONTRASEÑA ES NECESARIA PARA INGRESAR",Toast.LENGTH_SHORT).show();
+                }else if(txtPassUserLU.getText().length() < 3){
+                    Toast.makeText(getApplicationContext(),"SU CONTRASEÑA NO PUEDE SER MENOR A TRES LETRAS",Toast.LENGTH_SHORT).show();
                 }else {
-                    getUsuaioL();
+                    getPass();
                 }
+
             }
         });
+
     }
 
     /******************GET A LA BD***********************************/
-    public void getUsuaioL() {
-        user = txtUserL.getText().toString().toUpperCase();
-        pass = txtPassL.getText().toString().toUpperCase();
+    public void getPass() {
+        user = cargarInfoUser;
+        pass = txtPassUserLU.getText().toString();
+        pass = pass.trim();
 
         final OkHttpClient client = new OkHttpClient();
         final Request request = new Request.Builder()
-                .url("http://187.174.102.142/AppTransito/api/Usuarios?user="+user+"&pass="+pass)
+                .url("http://187.174.102.142/AppTransito/api/Usuarios?nombre="+user+"&pass="+pass)
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -83,15 +86,15 @@ public class Login extends AppCompatActivity {
                     String valorUser = "false";
                     if(resp.equals(valorUser)){
                         Looper.prepare(); // to be able to make toast
-                        Toast.makeText(getApplicationContext(), "LO SENTIMOS, USUARIO NO REGISTRADO", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "LO SENTIMOS, CONTRASEÑA INCORRECTA", Toast.LENGTH_SHORT).show();
                         Looper.loop();
                     }else{
-                        bandera = 1;
-                        respUser = resp;
+                        banderaLogin = 2;
+                        respPass = resp;
                         Looper.prepare(); // to be able to make toast
-                        guardarDatosUser();
+                        guardarDatosUserLogin();
                         Toast.makeText(getApplicationContext(), "ESTAMOS PROCESANDO SU SOLICITUD, UN MOMENTO POR FAVOR", Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(Login.this,LoginUser.class);
+                        Intent i = new Intent(LoginUser.this,Reglamento.class);
                         startActivity(i);
                         finish();
                         Looper.loop();
@@ -103,11 +106,15 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    private void guardarDatosUser(){
+    public void cargarDatos(){
+        share = getSharedPreferences("main",MODE_PRIVATE);
+        cargarInfoUser = share.getString("USER","");
+    }
+
+    private void guardarDatosUserLogin(){
         share = getSharedPreferences("main",MODE_PRIVATE);
         editor = share.edit();
-        editor.putString("USER",respUser);
-        editor.putInt("STATUS",bandera);
+        editor.putInt("STATUSUSER",banderaLogin);
         editor.commit();
     }
 }
--- app/src/main/java/mx/hgo/reglamento/Splash.java
+++ app/src/main/java/mx/hgo/reglamento/Splash.java
@@ -3,21 +3,47 @@ package mx.hgo.reglamento;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
+    String cargarInfoUsuario;
+    int cargarInfoStatus,cargarInfoStatusLogin;
+    /*********** SHARE PREFERENCE ************/
+    SharedPreferences share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        cargarDatos();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Splash.this,Login.class);
-                startActivity(i);
+                if (cargarInfoUsuario.isEmpty() != false) {
+                    Intent i = new Intent(Splash.this, Login.class);
+                    startActivity(i);
+                    finish();
+                } else if (cargarInfoStatusLogin == 2) {
+                    Intent i = new Intent(Splash.this, Reglamento.class);
+                    startActivity(i);
+                    finish();
+                } else if (cargarInfoStatus == 1) {
+                    Intent i = new Intent(Splash.this, LoginUser.class);
+                    startActivity(i);
+                    finish();
+                }
             }
         },2000);
     }
+
+    public void cargarDatos(){
+        share = getSharedPreferences("main",MODE_PRIVATE);
+        cargarInfoUsuario = share.getString("USER","");
+        cargarInfoStatus = share.getInt("STATUS",0);
+        cargarInfoStatusLogin = share.getInt("STATUSUSER",0);
+    }
+
 }
