75fba55937878d36ea03090bccc4b0c415e80f69
Karlita <karlis58rg@gmail.com>
2020-05-21 01:45:05 -0500

    splash

--- app/src/main/java/mx/hgo/reglamento/Login.java
+++ app/src/main/java/mx/hgo/reglamento/Login.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
