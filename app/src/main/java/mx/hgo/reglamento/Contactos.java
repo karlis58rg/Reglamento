98d11e084c36f4c4549bde2e00dd6a8a8da267dc
Karlita <karlis58rg@gmail.com>
2020-05-25 10:06:41 -0500

    eliminando_vista_contactos

--- app/src/main/java/mx/hgo/reglamento/Contactos.java
+++ app/src/main/java/mx/hgo/reglamento/Contactos.java
@@ -2,53 +2,13 @@ package mx.hgo.reglamento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class Contactos extends AppCompatActivity {
-    private LinearLayout btnReglamento,btnLugaresPago,btnMenu,btnTabulador;
-    private ImageView btnList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos);
-
-        btnReglamento = findViewById(R.id.lyInicio);
-        btnLugaresPago = findViewById(R.id.lyCategoria);
-        btnMenu = findViewById(R.id.lyContacto);
-        btnTabulador = findViewById(R.id.lyFavoritos);
-        btnList = findViewById(R.id.btnList);
-
-        btnReglamento.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Contactos.this, ViewPDFController.class);
-                startActivity(i);
-            }
-        });
-        btnLugaresPago.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Contactos.this, LugaresDePago.class);
-                startActivity(i);
-            }
-        });
-        btnTabulador.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        btnList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Contactos.this, Reglamento.class);
-                startActivity(i);
-            }
-        });
     }
 }
